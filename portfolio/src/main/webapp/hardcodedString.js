50716c062a55fc09d8d7168ae1952d9d0d9d3dbf
Diego Gomez Cota <gomezcotadiego@gmail.com>
2021-03-05 06:59:30 +0000

    Add servlet that serves the hardcoded message

--- portfolio/src/main/webapp/hardcodedString.js
+++ portfolio/src/main/webapp/hardcodedString.js
@@ -1,6 +1,6 @@
 /** Fetches the hard-coded string from the server and adds it to the page. */
 async function showHardcodedString() {
-    const responseFromServer = await fetch('/hello-world');
+    const responseFromServer = await fetch('/get-message');
     const textFromResponse = await responseFromServer.text();
 
     const msgContainer = document.getElementById('server-message-container');
