34d3210a54a18e8b8ee248f362e50791bdc5d9a7
Diego Gomez Cota <gomezcotadiego@gmail.com>
2021-03-15 01:08:30 +0000

    Add servlet that allows retrieving images grouped by tags

--- portfolio/src/main/java/com/google/sps/servlets/ImageAnalysisFormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ImageAnalysisFormHandlerServlet.java
@@ -14,6 +14,24 @@
 
 package com.google.sps.servlets;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
@@ -26,36 +44,7 @@ import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.datastore.v1.Value;
 import com.google.protobuf.ByteString;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.FullEntity;
-import com.google.cloud.datastore.KeyFactory;
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
-import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.QueryResults;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
-import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 
 /**
  * Takes the image uploaded by the user, stores it in Cloud Storage, and then extracts labels using
@@ -84,23 +73,28 @@ public class ImageAnalysisFormHandlerServlet extends HttpServlet {
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Label");
 
+    long timestamp = System.currentTimeMillis();
+
     // Store labels and image in Datastore
     for (EntityAnnotation label : imageLabels) {
         // agregarla a la lista de labels si es que no existe
         String description = label.getDescription();
         FullEntity labelEntity =
             Entity.newBuilder(keyFactory.newKey())
-                .set("id", description)
+                .set("description", description)
+                .set("timestamp", timestamp)
                 .build();
         datastore.put(labelEntity);
         // agregar la imagen a su Kind
         KeyFactory keyFactoryLabel = datastore.newKeyFactory().setKind(description);
         FullEntity imageEntity = 
             Entity.newBuilder(keyFactoryLabel.newKey())
-                .set("img", imageUrl)
+                .set("imageUrl", imageUrl)
+                .set("timestamp", timestamp)
                 .build();
         datastore.put(imageEntity);
      }
+     response.sendRedirect("/index.html");
    }
 
   /**
